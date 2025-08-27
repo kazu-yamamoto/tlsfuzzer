894634d5efe4b2b5e733d016949eb454c69e2bf4
Kazu Yamamoto <kazu@iij.ad.jp>
2025-08-27 15:50:01 +0900

    test_tls13_large_number_of_extensions.py

--- tlsfuzzer/_apps/test_tls13_large_number_of_extensions.py
+++ tlsfuzzer/_apps/test_tls13_large_number_of_extensions.py
@@ -164,11 +164,11 @@ def main():
     unassigned_ext_id.extend(range(2, 5))
     unassigned_ext_id.extend(range(6, 10))
     unassigned_ext_id.extend([11, 12, 17])
-    unassigned_ext_id.extend(range(22, 28))
+    unassigned_ext_id.extend(range(22, 26))
     unassigned_ext_id.extend(range(29, 41))
     unassigned_ext_id.extend([46])
     unassigned_ext_id.extend(range(52, 57))
-    unassigned_ext_id.extend(range(58, 65536))
+    unassigned_ext_id.extend(range(58, 64767))
 
     # Exclude extensions from a list of unassigned ones
     unassigned_ext_id = [ext for ext in unassigned_ext_id if ext not in ext_exclude]
